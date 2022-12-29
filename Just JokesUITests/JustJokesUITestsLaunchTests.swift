31b2919fe1021d358bd348184392e0ecd844de18
Grant McNally <grantmcnally@gmail.com>
2022-12-29 11:32:03 +1100

    Add SwiftLint & fix warnings and errors.

--- Just JokesUITests/JustJokesUITestsLaunchTests.swift
+++ Just JokesUITests/JustJokesUITestsLaunchTests.swift
@@ -7,7 +7,7 @@
 
 import XCTest
 
-final class Just_JokesUITestsLaunchTests: XCTestCase {
+final class JustJokesUITestsLaunchTests: XCTestCase {
 
     override class var runsForEachTargetApplicationUIConfiguration: Bool {
         true
